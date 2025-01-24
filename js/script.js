f00d79b49a71faa56dabceb1b758ba26d660d8f2
Eleazarovich <eleazarovich176@gmail.com>
2025-01-24 13:15:26 +0200

    complete basic core functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-console.log("Hello, World!");
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     return Math.floor(Math.random() * (4 - 1) + 1);
@@ -8,15 +9,63 @@ function getHumanChoice() {
     const humanChoice = prompt("rock, paper, scissors").toLocaleLowerCase();
 
     if (humanChoice == "rock") {
-        return humanChoice;
+        return 1;
     } else if (humanChoice == "paper") {
-        return humanChoice;
+        return 2;
     } else if (humanChoice == "scissors") {
-        return humanChoice;
+        return 3;
     } else {
-        return "invalid choice"
+        return -1;
     }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, compterChoice) {
+    if (humanChoice == 3 && compterChoice == 2) {
+        return "You win! Scissors beats Paper";
+    } else if (humanChoice == 2 && compterChoice == 1) {
+        return "You win! Paper beats Rock";
+    } else if (humanChoice == 1 && compterChoice == 3) {
+        return "You win! Rock beats Scissors";
+    } else if (humanChoice == 2 && compterChoice == 3) {
+        return "You lose! Scissors beats Paper"
+    } else if (humanChoice == 1 && compterChoice == 2) {
+        return "You lose! Paper beats Rock"
+    } else if (humanChoice == 3 && compterChoice == 1) {
+        return "You lose! Rock beats Scissors";
+    } else {
+        return "It's a tie!";
+    }
+}
+
+function getWinnerPerRound(roundResult) {
+    if (roundResult.startsWith("You win!")) {
+        humanScore += 1;
+        return roundResult;
+    } else if (roundResult.startsWith("You lose!")) {
+        computerScore +=1;
+        return roundResult;
+    } else {
+        return roundResult;
+    }
+}
+
+function showOverallWinner() {
+    if (humanScore > computerScore) {
+        console.log(`You won! ${humanScore} to ${computerScore} agaist the computer.`);
+    } else {
+        console.log(`You lost! ${humanScore} to ${computerScore} agaist the computer.`);
+    }
+}
+
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        const getRoundResult = playRound(humanSelection, computerSelection);
+        const showWinnerCurrentRound = getWinnerPerRound(getRoundResult);
+        console.log(showWinnerCurrentRound);
+    }
+    showOverallWinner();
+}
+
+playGame();
